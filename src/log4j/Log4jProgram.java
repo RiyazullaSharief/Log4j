9a3dadb439cdc345ae6f3161c2cd57d2958f5a95
Riyazulla Sharief <Riyazulla Sharief@RIYAZ-PC>
2018-12-30 00:41:09 +0530

    Signed-off-by: Riyazulla Sharief <Riyazulla Sharief@RIYAZ-PC>

---
+++ src/log4j/Log4jProgram.java
@@ -0,0 +1,12 @@
+package log4j;
+
+import org.apache.log4j.Logger;
+
+public class Log4jProgram
+{
+	public static void main(String[] args)
+	{
+		Logger log=Logger.getLogger("NewLog");
+		log.info("Log4j is working...");
+	}
+}
